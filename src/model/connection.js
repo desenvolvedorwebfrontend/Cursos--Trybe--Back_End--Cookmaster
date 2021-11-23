7219c5d207d42a6a5a77abd78eae33324eb61513
Marcos Albuquerque <mpdsa20@gmail.com>
2021-11-23 08:45:33 -0300

    codigos quebrados

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; 
+// const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
 const DB_NAME = 'Cookmaster';
 const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 let db = null;
